332657c41573f0247afdad4c431179733ae88a95
mmmfang <mae.fang@gmail.com>
2021-03-04 00:04:44 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,11 +11,12 @@ var lyric = "maybe";
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
